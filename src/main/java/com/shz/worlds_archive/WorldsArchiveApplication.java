c12cd21f4b741e9f2c708ed6a6251b7adc79d140
MichaelSuthirta <michaelsuthirta@gmail.com>
2026-02-26 22:00:17 +0700

    Feat: Init commit

---
+++ src/main/java/com/shz/worlds_archive/WorldsArchiveApplication.java
@@ -0,0 +1,13 @@
+package com.shz.worlds_archive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldsArchiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldsArchiveApplication.class, args);
+	}
+
+}
