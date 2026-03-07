bfffdaee72a390830075bb30061214cf20854d36
MichaelSuthirta <michaelsuthirta@gmail.com>
2026-03-07 16:49:32 +0700

    Feat: Created main menu UI

---
+++ src/main/java/com/shz/worlds_archive/controllers/NavigationController.java
@@ -0,0 +1,21 @@
+package com.shz.worlds_archive.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import ch.qos.logback.core.model.Model;
+import jakarta.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author aeryxx
+ */
+
+@Controller
+public class NavigationController {
+    @RequestMapping(value="/", method=RequestMethod.GET)
+    public String goToMenu(HttpServletRequest request, Model model){
+        return "menu";
+    }
+}
